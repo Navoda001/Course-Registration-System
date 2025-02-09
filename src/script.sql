632c08f6a42fe25413e9f724dd20898d766a6f86
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-02-10 00:47:13 +0530

    update Script.sql file

--- src/script.sql
+++ src/script.sql
@@ -5,104 +5,105 @@ USE course_registration_system;
 
 #====================================================================================================
 
-DROP TABLE IF EXISTS Student;
-CREATE TABLE student (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    studentId VARCHAR(20) GENERATED ALWAYS AS (CONCAT('S-', LPAD(Id, 3, '0'))) STORED,
+DROP TABLE IF EXISTS students;
+CREATE TABLE students (
+    studentId INT AUTO_INCREMENT PRIMARY KEY,
     studentName VARCHAR(100) NOT NULL,
     dateOfBirth DATE NOT NULL,
-    programOfStudy VARCHAR(100) ,
+    programOfStudy VARCHAR(100),
     year INT CHECK (year BETWEEN 1 AND 8) NOT NULL,
     contactInfo VARCHAR(50) NOT NULL
-);
+) AUTO_INCREMENT = 1000;
+
 SHOW TABLES ;
-DESCRIBE Student;
+DESCRIBE student;
 
 #====================================================================================================
-DROP TABLE IF EXISTS Course;
-CREATE TABLE Course (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    courseId VARCHAR(20) GENERATED ALWAYS AS (CONCAT('C-', LPAD(id, 3, '0'))) STORED,
+DROP TABLE IF EXISTS courses;
+CREATE TABLE courses (
+    courseId INT AUTO_INCREMENT PRIMARY KEY,
     courseTitle VARCHAR(50) NOT NULL,
     creditHours INT NOT NULL,
     department VARCHAR(50) NOT NULL,
-    prerequisites VARCHAR(50) NOT NULL,
+    prerequisites VARCHAR(50),
     enrollmentCapacity INT NOT NULL
-);
+) AUTO_INCREMENT = 1000;
 
 SHOW TABLES ;
-DESCRIBE Course;
+DESCRIBE course;
 
 #====================================================================================================
 
-DROP TABLE IF EXISTS AcademicRecord;
-CREATE TABLE academicRecord (
+DROP TABLE IF EXISTS academic_records;
+CREATE TABLE academic_records (
     studentId INT NOT NULL,
     courseId INT NOT NULL,
     semester VARCHAR(20) NOT NULL,
     grade CHAR(2) NOT NULL,
-    PRIMARY KEY (student_id, course_id,semester), 
-    FOREIGN KEY (studentId) REFERENCES Student(studentId) ON DELETE CASCADE,
-    FOREIGN KEY (courseId) REFERENCES Course(courseId) ON DELETE CASCADE 
+    PRIMARY KEY (studentId, courseId, semester), 
+    FOREIGN KEY (studentId) REFERENCES Students(studentId) ON DELETE CASCADE,
+    FOREIGN KEY (courseId) REFERENCES courses(courseId) ON DELETE CASCADE
 );
 
 SHOW TABLES ;
-DESCRIBE AcademicRecord;
+DESCRIBE academic_records;
 
 #====================================================================================================
 
-CREATE TABLE Enrollments (
-    studentId INT,
-    courseId INT,
+DROP TABLE IF EXISTS enrollments;
+CREATE TABLE enrollments (
+    studentId INT NOT NULL,
+    courseId INT NOT NULL,
     semester VARCHAR(20) NOT NULL,
     enrollmentDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (studentId, courseId, semester),
     FOREIGN KEY (studentId) REFERENCES Students(studentId) ON DELETE CASCADE,
-    FOREIGN KEY (courseId) REFERENCES Courses(courseId) ON DELETE CASCADE
+    FOREIGN KEY (courseId) REFERENCES courses(courseId) ON DELETE CASCADE
 );
 
 SHOW TABLES ;
-DESCRIBE Enrollments;
+DESCRIBE enrollments;
 
 #====================================================================================================
-
-CREATE TABLE Users (
-    userId INT PRIMARY KEY AUTO_INCREMENT,
-    userName VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL, -- Store hashed passwords
-    role ENUM('student', 'faculty', 'admin') NOT NULL,
-    studentId INT UNIQUE, -- Null for non-students
-    facultyId INT UNIQUE, -- Null for non-faculty
-    adminId INT UNIQUE, -- Null for non-admins
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (studentId) REFERENCES Students(studentId) ON DELETE SET NULL,
-    FOREIGN KEY (facultyId) REFERENCES Faculty(facultyId) ON DELETE SET NULL,
-    FOREIGN KEY (adminId) REFERENCES Admin(adminId) ON DELETE SET NULL
-);
-
-SHOW TABLES ;
-DESCRIBE Users;
-
-#====================================================================================================
-
-CREATE TABLE Faculty (
+DROP TABLE IF EXISTS faculties;
+CREATE TABLE faculties (
     facultyId INT PRIMARY KEY AUTO_INCREMENT,
     facultyName VARCHAR(100) NOT NULL,
     contactInfo VARCHAR(255)
-);
+)AUTO_INCREMENT = 1000;
 
 SHOW TABLES ;
-DESCRIBE Faculty;
+DESCRIBE faculties;
+
+#====================================================================================================
+DROP TABLE IF EXISTS admins;
+CREATE TABLE admins (
+    adminId INT PRIMARY KEY AUTO_INCREMENT,
+    adminName VARCHAR(100) NOT NULL,
+    facultyId INT,
+    department VARCHAR(100) ,
+    contact_info VARCHAR(255),
+    FOREIGN KEY (facultyId) REFERENCES faculties(facultyId) ON DELETE SET NULL
+)AUTO_INCREMENT = 1000;
+
+SHOW TABLES ;
+DESCRIBE admins;
 
 #====================================================================================================
 
-CREATE TABLE Admin (
-    adminId INT PRIMARY KEY AUTO_INCREMENT,
-    Adminame VARCHAR(100) NOT NULL,
-    facultyId VARCHAR(100) ,
-    department VARCHAR(100) ,
-    contact_info VARCHAR(255)
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    userId INT PRIMARY KEY AUTO_INCREMENT,
+    userName VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL, 
+    entityId INT NOT NULL,  
+    entityType ENUM('student', 'faculty', 'admin'), 
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-SHOW TABLES ;
-DESCRIBE Admin;
+SHOW TABLES;
+DESCRIBE Users;
+
+
+
